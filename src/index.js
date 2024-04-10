bc637f250b7e88085dce5e41c844f83f84b0d032
IkerOcon <iker.ocon@540deg.com>
2024-04-10 13:04:03 +0200

    Configurar y crear un servidor web utilizando Fastify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const Fastify = require('fastify');
+const App = require('./app');
+
+async function start() {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    await fastify.register(App);
+
+    await fastify.listen({
+        host: '0.0.0.0',
+        port: 8081,
+    });
+}
+
+start().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
