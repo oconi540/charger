ebe3e4302651c45cb7803c94377d3c1703fccf8f
IkerOcon <iker.ocon@540deg.com>
2024-04-12 12:20:16 +0200

    Eliminar @fastify/sensible

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 const { FastifyInstance } = require('fastify');
-const Sensible = require('@fastify/sensible');
 const knexConfig = require('../knexfile');
 const getCharger = require('./GETcharger');
 
 async function app(fastify, opts) {
-    await fastify.register(Sensible);
-
-    fastify.route({
-        url: '/charger/:id',
-        method: 'GET',
-        handler: getCharger,
-    });
+    fastify.get('/charger/:id', getCharger);
 }
 
 module.exports = app;
