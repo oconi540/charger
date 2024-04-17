1b3590338c98b152a46294ae7df5a6049be42f48
oconi540 <165193074+oconi540@users.noreply.github.com>
2024-04-17 14:11:19 +0200

    Revert "8 migrar de javascript a typescript (#9)"
    This reverts commit c2068441f913fd33dd819d8eb5b0f57a496f2a59.

---
+++ db/migrations/20240410152948_add_charger_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    return knex.schema.createTable('charger', function (table) {
+        table.increments('id').primary();
+        table.string('alias').notNullable();
+        table.string('model').notNullable();
+        table.integer('serialNumber').notNullable().unique();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    return knex.schema.dropTable('charger');
+};
