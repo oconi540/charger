f9ed282b52be97503474cbe2d07157527a8a957b
IkerOcon <iker.ocon@540deg.com>
2024-04-17 13:58:03 +0200

    Modificar scripts migrations/seeds

--- knexfile.ts
+++ knexfile.ts
@@ -1,28 +1,32 @@
+import path from 'path';
 import * as dotenv from 'dotenv';
 import { Knex } from 'knex';
 
 dotenv.config();
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 const knexConfig: { [key: string]: Knex.Config } = {
-    development: {
-        client: 'pg',
-        connection: {
-            connectionString: process.env.DATABASE_URL as string,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            extension: 'ts',
-            directory: __dirname + '/db/migrations',
-            tableName: 'knex_migrations',
-        },
-        seeds: {
-            extension: 'ts',
-            directory: __dirname + '/db/seeds',
-        },
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL as string,
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      extension: 'ts',
+      directory: path.join(__dirname, 'db', 'migrations'),
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      extension: 'ts',
+      directory: path.join(__dirname, 'db', 'seeds'),
+    },
+  },
 };
 
 export default knexConfig;
